090f828037161079eea400254d29ea2121ad1b01
unknown <saiftony5@gmail.com>
2021-11-08 18:37:52 +0100

    mmmmmmmm

--- arduino/settings.py
+++ arduino/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--al3^0q$w88#@2eqi9t#%0xao00v(sm&jmsvk)lf@%89i+nsw='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 

3fefd2eb6c7ada9dc63d9f69061dffee13408ef5
unknown <saiftony5@gmail.com>
2021-11-08 14:51:46 +0100

    modi

--- arduino/settings.py
+++ arduino/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-from environs import Env # new
-env = Env() # new
-env.read_env() # new
+# from environs import Env # new
+# env = Env() # new
+# env.read_env() # new
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
